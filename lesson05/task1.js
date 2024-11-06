a0cf8fad1de42c8a2394d26f5a6669a88cbf5207
Bohdan Lutsyk <xnunrgx@gmail.com>
2024-11-07 00:57:45 +0200

    Add tasks solutions for lesson05 homwork.

---
+++ lesson05/task1.js
@@ -0,0 +1,48 @@
+// Завдання 1: Визначення рівня успішності студента
+
+// Студентам потрібно визначити свій рівень успішності за оцінками, які вони отримали за курс. Напишіть програму, яка допоможе студентам визначити свій рівень успішності на основі середньої оцінки.
+
+// Створіть змінну averageGrade і присвойте їй середню оцінку студента (від 0 до 100).
+// За допомогою конструкції if else, визначте та виведіть у консоль рівень успішності студента:
+// Якщо середня оцінка менше 60, вивести "Незадовільно".
+// Якщо середня оцінка від 60 до 70 (включно), вивести "Задовільно".
+// Якщо середня оцінка від 71 до 80 (включно), вивести "Добре".
+// Якщо середня оцінка від 81 до 90 (включно), вивести "Дуже добре".
+// Якщо середня оцінка від 91 до 100 (включно), вивести "Відмінно".
+// Приклад вхідних даних:
+// const averageGrade = 85;
+// Очікуваний результат:
+// Дуже добре
+
+const averageGrade = 0;
+
+if (0 <= averageGrade && averageGrade < 60) {
+    console.log(`Незадовільно: ${averageGrade} балів.`);
+} else if (60 <= averageGrade && averageGrade <= 70) {
+    console.log(`Задовільно: ${averageGrade} балів.`);
+} else if (70 < averageGrade && averageGrade <= 80) {
+    console.log(`Добре: ${averageGrade} балів.`);
+} else if (80 < averageGrade && averageGrade <= 90) {
+    console.log(`Дуже добре: ${averageGrade} балів.`);
+} else if (90 < averageGrade && averageGrade <= 100) {
+    console.log(`Відмінно: ${averageGrade} балів.`);
+} else {
+    console.log(`Оцінка не в діапазоні від 0 до 100.`);
+} 
+
+//  If we use if...else without Logical Operators than we will miss negative digit < 0 
+//  Negative digits always will be in condition < 60 and with message: `Незадовільно: ${averageGrade} балів.`
+//  not with message: `Оцінка не в діапазоні від 0 до 100.`
+//   if (averageGrade < 60) {
+//     console.log(`Незадовільно: ${averageGrade} балів.`);
+//   } else if (averageGrade <= 70) {
+//     console.log(`Задовільно: ${averageGrade} балів.`);
+//   } else if (averageGrade <= 80) {
+//     console.log(`Добре: ${averageGrade} балів.`);
+//   } else if (averageGrade <= 90) {
+//     console.log(`Дуже добре: ${averageGrade} балів.`);
+//   } else if (averageGrade <= 100) {
+//     console.log(`Відмінно: ${averageGrade} балів.`);
+//   } else {
+//     console.log(`Оцінка не в діапазоні від 0 до 100.`);
+//   } 
---
+++ lesson05/task3.js
@@ -0,0 +1,45 @@
+// Завдання 3: Генерація таблиці множення
+
+// Створіть програму, яка генерує таблицю множення для обраного числа за допомогою кожного з циклів for, while .
+// Виберіть число для якого потрібно згенерувати таблицю множення.
+// Використовуючи цикл for, створіть таблицю множення для обраного числа від 1 до 10. Виведіть результат в консоль.
+// Використовуючи цикл while створіть аналогічну таблицю множення та виведіть її в консоль.
+// Приклад вхідних даних:
+// const number = 5;
+// Очікуваний результат:
+// 5 x 1 = 5
+// 5 x 2 = 10
+// 5 x 3 = 15
+// 5 x 4 = 20
+// 5 x 5 = 25
+// 5 x 6 = 30
+// 5 x 7 = 35
+// 5 x 8 = 40
+// 5 x 9 = 45
+// 5 x 10 = 50
+
+const number = 10;
+
+console.log(`Таблиця множення числа ${number} використовуючи цикл 'for':`);
+
+for (let multiplier = 1; multiplier <= 10; multiplier++) {
+	console.log(`${number} x ${multiplier} = ${number * multiplier}`);
+}
+
+console.log(`\nТаблиця множення числа ${number} використовуючи цикл 'while':`);
+
+let multiplier = 1;
+while (multiplier <= 10) {
+    console.log(`${number} x ${multiplier} = ${number * multiplier}`);
+    multiplier++
+}
+
+console.log(`\nТаблиця множення чисел використовуючи цикли 'for' і 'while':`);
+for (let number = 1; number <= 10; number++) {
+    let multiplier = 1;
+    while (multiplier <= 10) {
+        console.log(`${number} x ${multiplier} = ${number * multiplier}`);
+        multiplier++
+    }
+    console.log('')
+}

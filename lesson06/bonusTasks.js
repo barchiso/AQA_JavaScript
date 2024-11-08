6cb44e13a87e53726e756bee942f8213d5ce507f
Bohdan Lutsyk <xnunrgx@gmail.com>
2024-11-09 00:19:27 +0200

    Add homewowork #6 tasks solution and solutions from the lectors tasks.

---
+++ lesson06/bonusTasks.js
@@ -0,0 +1,65 @@
+//  Задача #1. Функція додавання.
+//  Напишіть тіло функції add що приймає два числа в якості параметрів та має повертати їх суму.
+
+function add(a, b) {
+    return a + b 
+}
+
+//  Using separate Arrow function 
+const addArrow = (a, b) => a + b
+
+console.log(add(5, 7));
+console.log(add(0,-1));
+console.log(add(0, 0));
+console.log(add(-10, -8));
+console.log(add(-5, 5));
+
+console.log(addArrow(5, 7));
+console.log(addArrow(0,-1));
+console.log(addArrow(0, 0));
+console.log(addArrow(-10, -8));
+console.log(addArrow(-5, 5));
+
+//  Задача #2. Функція для визначення парних чисел.
+//  Імплементуйте функцію isEven, яка приймає число і повертає true, якщо число парне, і false, якщо число непарне.
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+//  Using separate Arrow function 
+const isEvenArrow = (n) => n % 2 === 0
+
+console.log(isEven(8));
+console.log(isEven(15));
+console.log(isEven(0));
+console.log(isEven(-16));
+console.log(isEven(-5));
+console.log(isEven(8.4));
+
+console.log(isEvenArrow(8));
+console.log(isEvenArrow(15));
+console.log(isEvenArrow(0));
+console.log(isEvenArrow(-16));
+console.log(isEvenArrow(-5));
+console.log(isEvenArrow(8.4));
+
+//  Задача #3. Функція для розвороту рядка.
+//  Напишіть тіло функції reverseString, яка приймає рядок і повертає його в зворотньому порядку.
+
+function reverseString(str) {
+    return  Array.from(str).reverse().join('');
+}
+
+//  Using separate Arrow function 
+const reverseStringArrow = (str) => Array.from(str).reverse().join('')
+
+console.log(reverseString(''));
+console.log(reverseString('a'));
+console.log(reverseString('Hello World!!!'));
+console.log(reverseString('!!!-Hello-World-$$$!!!'));
+
+console.log(reverseStringArrow(''));
+console.log(reverseStringArrow('a'));
+console.log(reverseStringArrow('Hello World!!!'));
+console.log(reverseStringArrow('!!!-Hello-World-$$$!!!'));
---
+++ lesson06/task2.js
@@ -0,0 +1,33 @@
+//  Завдання 2
+//  1. Створіть функцію яка приймає один параметр: age.
+//  2. Усередині функції перевірте, чи age більше або рівне 18.
+//  3. Якщо age відповідає умові, поверніть true, інакше поверніть false.
+//  4. Викличте вашу функцію з аргументами 25 і 15, і виведіть результат (чи є особа дорослою) в консоль для кожного випадку.
+
+//  Using function declaration.
+function isAdult (age) {
+    return  age >= 18;
+}
+
+console.log(isAdult(25));
+console.log(isAdult(15));
+console.log(isAdult(18));
+console.log(isAdult(17.5));
+
+//  Using function expression.
+const isAdultExpression = function(age) {
+    return age >= 18;
+}
+
+console.log(isAdultExpression(25));
+console.log(isAdultExpression(15));
+console.log(isAdultExpression(18));
+console.log(isAdultExpression(17.5));
+
+//  Better optimized for this function use Arrow function.
+const isAdultArrow = (age) => age >= 18;
+
+console.log(isAdultArrow(25));
+console.log(isAdultArrow(15));
+console.log(isAdultArrow(18));
+console.log(isAdultArrow(17.5));

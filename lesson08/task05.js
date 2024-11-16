788658867865d79092107552dc206b17a0a6fc49
Bohdan Lutsyk <xnunrgx@gmail.com>
2024-11-16 04:27:30 +0200

    Add homework 08 task solution. Add solutions for lectors tasks.

---
+++ lesson08/task05.js
@@ -0,0 +1,16 @@
+//  Завдання 5: Об'єднання масивів
+//  Вам потрібно створити програму, яка об'єднає два масиви в один
+//  Ось кроки, які вам потрібно виконати:
+//  1. Створіть два масиви, наприклад, firstArray і secondArray, які містять деякі значення.
+//  2. Створіть новий масив, який містить всі елементи з обох вихідних масивів.
+//  3. Виведіть отриманий об'єднаний масив на консоль.
+
+//  Create function to  concatenate two arrays into one.
+const concatArrays = (firstArray,  secondArray) => {
+    return firstArray.concat(secondArray);
+}
+
+const children = ["Cecile", "Lone", "Emil", "Tobias", "Linus"];
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+console.log(concatArrays(children, fruits));
---
+++ lesson08/task06.js
@@ -0,0 +1,18 @@
+//  Завдання 6: Сортування масивів 
+//  У вас є вихідний масив
+//  сonst numbersList = [1,10,14,2,4,5,43,34]
+//  1. Створіть новий масив на основі вихідного масиву (копіюйте його)
+//  2. Відсортуйте створений масив (від меншого до більшого)
+//  3. Виведіть обидва масиви в консоль
+
+//  Create function to sort array in ascending order.
+const sortedArray = (numbers) =>{
+    // a - first element.
+    // b - second element.
+    return numbers.sort(function(a, b){return a - b});
+}
+
+const numbersList = [1, 10, 14, 2, 4, 5, 43, 34];
+
+console.log(numbersList);
+console.log(sortedArray(numbersList));  //  [1,  2,  4,  5, 10, 14, 34, 43]

c71c705798ff655b2c1382efd556b7366c299bed
Bohdan Lutsyk <xnunrgx@gmail.com>
2024-12-11 00:10:13 +0200

    Lesson13 (#14)
    * Setup axios/jest env. Add lesson # 13 axios/jest tests. jsonPlaceholder.test.js
    
    * Update files to setup axios/jest env.

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,23 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import pluginJest from 'eslint-plugin-jest';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
-
 	{
-		// rules: {
-		//   'no-unused-vars': 'warn',
-		//   'no-undef': 'warn',
-		//   'no-console': 'warn'
-		// },
-		// ignores: ['*/test.js']
+		files: ['**/*.spec.js', '**/*.test.js'],
+		plugins: { jest: pluginJest },
+		languageOptions: {
+			globals: pluginJest.environments.globals.globals, // add global variables Jest
+		},
+		rules: {
+			'jest/no-disabled-tests': 'warn',
+			'jest/no-focused-tests': 'error',
+			'jest/no-identical-title': 'error',
+			'jest/prefer-to-have-length': 'warn',
+			'jest/valid-expect': 'error',
+		},
 	},
 ];

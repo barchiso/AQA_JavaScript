bf01ae3b7209ca2fc81f8f69a6bb076df20d7c5f
Bohdan Lutsyk <xnunrgx@gmail.com>
2024-12-11 05:44:44 +0200

    Update

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginJest from 'eslint-plugin-jest';
-import pluginJest from 'eslint-plugin-jest';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [

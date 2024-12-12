6372a8c3cae2883d3041c7d1afcbf85e9a7b12c9
Bohdan Lutsyk <xnunrgx@gmail.com>
2024-12-12 04:49:16 +0200

    Add/Update lesson13/bookStore.test.js

--- lesson13/bookstore.test.js
+++ lesson13/bookstore.test.js
@@ -1,21 +1,25 @@
+// Домашнє завдання:(замість дефолтного) - Додати контролери до API тестів
+// Створити нову гілку
+// Використовувати тести з минулої домашки(API запити до JSONPlaceholder) АБО Створити 5-10 нових тестів до сервісу https://bookstore.toolsqa.com/swagger/#/
+// У тестах робити перевірки через expect
+// Використовувати контролери у тестах
 // Написати 5 тестів на API сервісу
 // https://demoqa.com/books
 // https://bookstore.toolsqa.com/swagger/
-
 // user: JohnnyB
 // pass: Johnny&Bravo9
 
 import axios from 'axios';
 
 const isbn = '9781593277574';
-const isbnDel = '9781449325862';
 const userId = '1f3d4e8a-b912-49c0-a08e-959def7b4a6a';
+let token;
 
 describe(`Book store API`, () => {
-	describe.skip(`Users requests`, () => {
+	describe(`Users requests`, () => {
 		test('Get generated token', async () => {
 			const response = await axios.post(
-				'https://demoqa.com/Account/v1/GenerateToken', // calling the get API
+				'https://bookstore.toolsqa.com/Account/v1/GenerateToken', // calling the get API
 				{
 					userName: 'JohnnyB',
 					password: 'Johnny&Bravo9',
@@ -27,6 +31,7 @@ describe(`Book store API`, () => {
 				},
 			);
 			console.log(response.data.token);
+			token = response.data.token;
 			expect(response.status).toEqual(200); // asserting if the response code is 200 result
 			expect(response.data.status).toBe('Success'); // asserting if the response status is 'Success'
 			expect(response.data.result).toBe('User authorized successfully.'); // asserting if the response result is 'User authorized successfully.'
@@ -34,9 +39,16 @@ describe(`Book store API`, () => {
 	});
 
 	describe(`Books requests`, () => {
+		const axiosInstance = axios.create({
+			baseURL: 'https://bookstore.toolsqa.com/BookStore/v1',
+			validateStatus: function (status) {
+				return status;
+			},
+		});
+
 		test('Get all books info', async () => {
-			const response = await axios.get(
-				'https://bookstore.toolsqa.com/BookStore/v1/Books', // calling the get API
+			const response = await axiosInstance.get(
+				'/Books', // calling the get API
 				{
 					headers: {
 						'Content-Type': 'application/json',
@@ -51,19 +63,10 @@ describe(`Book store API`, () => {
 			expect(response.data.books[0].author).toBe('Richard E. Silverman'); // asserting if the book author to be '<expected text>.'
 			expect(response.data.books[0].publisher).toBe("O'Reilly Media"); // asserting if the book publisher to be '<expected text>.'
 		});
-	});
-
-	describe(`Book info requests`, () => {
-		const axiosInstance = axios.create({
-			baseURL: 'https://demoqa.com/BookStore/v1',
-			validateStatus: function (status) {
-				return status;
-			},
-		});
 
 		test('Get specific book info', async () => {
-			const response = await axios.get(
-				`https://bookstore.toolsqa.com/BookStore/v1/Book?ISBN=${isbn}`, // calling the get API
+			const response = await axiosInstance.get(
+				`/Book?ISBN=${isbn}`, // calling the get API
 				{
 					headers: {
 						'Content-Type': 'application/json',
@@ -78,47 +81,60 @@ describe(`Book store API`, () => {
 			expect(response.data.publisher).toBe('No Starch Press'); // asserting if the book publisher to be '<expected text>.'
 			expect(response.data.pages).toBe(352); // asserting if the book pages to be 352
 		});
-		test('Delete specific book info', async () => {
+
+		test('Delete all books', async () => {
 			const response = await axiosInstance.delete(
-				`https://demoqa.com/BookStore/v1/Book`, // calling the get API
-				{
-					isbn: `${isbnDel}`,
-					userId: `${userId}`,
-				},
+				`/Books?UserId=${userId}`,
 				{
 					headers: {
 						'Content-Type': 'application/json',
-						Authorization:
-							'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IkpvaG5ueUIiLCJwYXNzd29yZCI6IkpvaG5ueSZCcmF2bzkiLCJpYXQiOjE3MzM4Nzg3MjN9.eZBGVRI2GL2uv-yNLHEPHPH1T1VIVKWlivm_dxUyAxs',
+						Authorization: `Bearer ${token}`,
 					},
 				},
 			);
-			console.log(JSON.stringify(response.data));
-			expect(response.status).toEqual(401); // asserting if the response code is 204
+			console.log(response.data);
+			expect(response.status).toEqual(204);
 		});
 
 		test('Add specific book to a user', async () => {
-			const response = await axios.post(
-				`https://bookstore.toolsqa.com/BookStore/v1/Books`,
+			const response = await axiosInstance.post(
+				`/Books`,
 				{
 					userId: `${userId}`,
 					collectionOfIsbns: [
 						{
-							isbn: `${isbnDel}`,
+							isbn: `${isbn}`,
 						},
 					],
 				},
 				{
 					headers: {
 						'Content-Type': 'application/json',
-						Authorization:
-							'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IkpvaG5ueUIiLCJwYXNzd29yZCI6IkpvaG5ueSZCcmF2bzkiLCJpYXQiOjE3MzM4Nzg3MjN9.eZBGVRI2GL2uv-yNLHEPHPH1T1VIVKWlivm_dxUyAxs',
+						Authorization: `Bearer ${token}`,
 					},
 				},
 			);
 			console.log(response.data);
 			expect(response.status).toEqual(201); // asserting if the response code is 200
-			expect(response.data.books[0].isbn).toBe(`${isbnDel}`);
+			expect(response.data.books[0].isbn).toBe(`${isbn}`);
+		});
+
+		test('Delete specific book info', async () => {
+			const response = await axiosInstance.delete(
+				`/Book`, // using the correct endpoint
+				{
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${token}`,
+					},
+					data: {
+						isbn: `${isbn}`,
+						userId: `${userId}`,
+					},
+				},
+			);
+			console.log(response.data);
+			expect(response.status).toEqual(204); // asserting if the response code is 204
 		});
 	});
 });
